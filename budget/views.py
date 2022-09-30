9e07caef726d3636f521117ecee4c2abcdf282ea
raig1988 <raig.iglesias@gmail.com>
2022-09-29 20:46:52 -0500

    Added html,css for register, login and added footer

--- budget/views.py
+++ budget/views.py
@@ -1,7 +1,13 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def index(request):
     return render(request, "budget/index.html")
+
+def register(request):
+    return render(request, "budget/register.html")
+
+def login(request):
+    return render(request, "budget/login.html")
